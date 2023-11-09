d7cd9d3bae98f16dac9081fbb286b9d1989e7ca7
Shawn Thomas <sljt@proton.me>
2023-11-08 19:57:34 -0500

    create test route to validate db connection

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 "use strict";
 
-/** Express app for Invi */
-
 const express = require("express");
+const app = express();
 const cors = require("cors");
+const db = require("./db");
+const { NotFoundError } = require("./expressError");
 
+// Middleware
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
+
+// Test Route
+app.get('/test', async (req, res) => {
+  try {
+    const result = await db.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Database error:", error); // Log the error for debugging
+    res.status(500).json({ error: "Internal server error" }); // Send a generic error message
+  }
+});
+
+// Handle 404 errors
+app.use(function (req, res, next) {
+  const err = new NotFoundError("Not Found");
+  return next(err);
+});
+
+// Global error handler
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message || "Internal server error";
+
+  if (process.env.NODE_ENV !== "test") {
+    console.error(status, err.stack);
+  }
+
+  res.status(status).json({ error: { message, status } });
+});
+
+module.exports = app;
