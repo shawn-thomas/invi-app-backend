07ad6b498186d2d5ff1364b12b5470f9fcd1790d
Shawn Thomas <sljt@proton.me>
2023-11-10 17:57:14 -0500

    Export SECRET_KEY

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret"
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 module.exports = {
+  SECRET_KEY,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri
 };
